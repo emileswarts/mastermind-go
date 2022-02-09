7ce16f9613387e842bc80ba29b42b4532c5bec94
Emile <emile.swarts@digital.justice.gov.uk>
2022-02-09 10:34:50 +0000

    Move board test to main file

--- src/main_test.go
+++ src/main_test.go
@@ -12,3 +12,58 @@ func TestCPUCCreatehallengeRow(t *testing.T) {
 	assert.Equal(t, challengeRow[0].x, 0)
 	assert.Equal(t, challengeRow[0].y, 11)
 }
+
+func TestGenerateEmptyBoard(t *testing.T) {
+	occupied_cells := make([]cell, 0)
+	empty_board := generate_board(occupied_cells)
+
+	assert.Equal(t, 0, empty_board[0].x)
+	assert.Equal(t, 0, empty_board[0].y)
+	assert.Equal(t, "", empty_board[0].colour)
+
+	assert.Equal(t, 1, empty_board[21].x)
+	assert.Equal(t, 4, empty_board[21].y)
+	assert.Equal(t, "", empty_board[21].colour)
+
+	assert.Equal(t, 4, empty_board[64].x)
+	assert.Equal(t, 12, empty_board[64].y)
+	assert.Equal(t, "", empty_board[64].colour)
+}
+
+func TestGenerateBoardWithOneColour(t *testing.T) {
+	occupied_cells := []cell{
+		{0, 0, "blue"},
+	}
+
+	empty_board := generate_board(occupied_cells)
+
+	assert.Equal(t, 0, empty_board[0].x)
+	assert.Equal(t, 0, empty_board[0].y)
+	assert.Equal(t, "blue", empty_board[0].colour)
+
+	assert.Equal(t, 1, empty_board[21].x)
+	assert.Equal(t, 4, empty_board[21].y)
+	assert.Equal(t, "", empty_board[21].colour)
+
+	assert.Equal(t, 4, empty_board[64].x)
+	assert.Equal(t, 12, empty_board[64].y)
+	assert.Equal(t, "", empty_board[64].colour)
+}
+
+func TestGenerateBoardWithMultipleColours(t *testing.T) {
+	occupied_cells := []cell{
+		{0, 0, "green"},
+		{1, 0, "white"},
+		{2, 0, "brown"},
+	}
+
+	empty_board := generate_board(occupied_cells)
+
+	assert.Equal(t, "green", empty_board[0].colour)
+	assert.Equal(t, "white", empty_board[1].colour)
+	assert.Equal(t, "brown", empty_board[2].colour)
+
+	assert.Equal(t, 4, empty_board[64].x)
+	assert.Equal(t, 12, empty_board[64].y)
+	assert.Equal(t, "", empty_board[64].colour)
+}
