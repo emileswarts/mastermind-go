9325fc0e7b38d918c0e8e7519ff3ca1007fde172
Emile <emile.swarts@digital.justice.gov.uk>
2022-02-09 14:49:27 +0000

    Ability to score a row with right colours but wrong positions

--- src/scorer.go
+++ src/scorer.go
@@ -1,16 +1,27 @@
 package main
 
+func rightColourWrongPosition(cpuCell cell, cellsForRow []cell) bool {
+	for _, cell := range cellsForRow {
+		if cell.colour == cpuCell.colour {
+			return true
+		}
+	}
+
+	return false
+}
+
 func scoreRow(currentBoard []cell, rowToTest int) []string {
 	var rowResult = make([]string, 5)
 	cellsForRow := findCellsInRow(currentBoard, rowToTest)
+	cpuCells := findCPUCells(currentBoard)
 
-	for i := 0; i < 5; i++ {
+	for i, cpuCell := range cpuCells {
 		if currentBoard[i].colour == "" {
 			rowResult[0] = "CPU row not set"
-		} else {
-			if cellsForRow[i].colour == findCPUCells(currentBoard)[i].colour {
-				rowResult[i] = "black"
-			}
+		} else if cellsForRow[i].x == cpuCell.x && cellsForRow[i].colour == cpuCell.colour {
+			rowResult[i] = "black"
+		} else if rightColourWrongPosition(cpuCell, cellsForRow) {
+			rowResult[i] = "white"
 		}
 	}
 
--- src/scorer_test.go
+++ src/scorer_test.go
@@ -16,7 +16,7 @@ func TestScoreWhenCPUNotSet(t *testing.T) {
 	assert.Equal(t, expectedResult[0], "CPU row not set")
 }
 
-func TestScoreWhenCPUSet(t *testing.T) {
+func TestWinningScore(t *testing.T) {
 	var expectedResult []string
 
 	occupied_cells := []cell{
@@ -39,4 +39,29 @@ func TestScoreWhenCPUSet(t *testing.T) {
 	assert.Equal(t, expectedResult[2], "black")
 	assert.Equal(t, expectedResult[3], "black")
 	assert.Equal(t, expectedResult[4], "black")
+}
+
+func TestRightColoursWrongPlacementScore(t *testing.T) {
+	var expectedResult []string
+
+	occupied_cells := []cell{
+		{0, 0, "green"},
+		{1, 0, "white"},
+		{2, 0, "brown"},
+		{3, 0, "blue"},
+		{4, 0, "black"},
+		{0, 12, "black"},
+		{1, 12, "green"},
+		{2, 12, "blue"},
+		{3, 12, "white"},
+		{4, 12, "brown"},
+	}
+	board := generate_board(occupied_cells)
+	expectedResult = scoreRow(board, 12)
+
+	assert.Equal(t, "white", expectedResult[0])
+	assert.Equal(t, "white", expectedResult[1])
+	assert.Equal(t, "white", expectedResult[2])
+	assert.Equal(t, "white", expectedResult[3])
+	assert.Equal(t, "white", expectedResult[4])
 }
