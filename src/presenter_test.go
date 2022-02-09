4bb4f2ed3b0fdc6804bb8d314aaa8f5d2144623e
Emile <emile.swarts@digital.justice.gov.uk>
2022-02-09 10:37:48 +0000

    Presenter can return an empty string

--- src/presenter_test.go
+++ src/presenter_test.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPresentBoard(t *testing.T) {
+	occupied_cells := []cell{
+		{0, 0, "green"},
+		{1, 0, "white"},
+		{2, 0, "brown"},
+	}
+
+	expectedResult := ""
+	board := generate_board(occupied_cells)
+	renderedBoard := render(board)
+
+	assert.Equal(t, expectedResult, renderedBoard)
+}
